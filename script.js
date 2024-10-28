1298b315fcb650dfaa52ba752237d8aafc2bd500
arshad012 <forarshad99@gmail.com>
2024-10-29 00:21:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+
+const strong_tags = document.querySelectorAll('#para > strong');
+
 function highlight() {
     //Write your code here
-
-
+	strong_tags.forEach((tag) => {
+		tag.classList.remove('black');
+		tag.classList.add('green');
+	})
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	strong_tags.forEach((tag) => {
+		tag.classList.remove('green');
+		tag.classList.add('black');
+	})
 }
